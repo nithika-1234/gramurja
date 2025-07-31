259806beebdb5618f3fcdecd9b9738f2738f4e6b
nithika-1234 <nithikam28@gmail.com>
2025-07-31 11:18:48 +0530

    Customer feedback

--- app/customer/CustomerHome.tsx
+++ app/customer/CustomerHome.tsx
@@ -185,6 +185,222 @@
 //   center: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 // });
 
+
+
+
+
+
+
+//2nd
+
+// import { useRouter } from 'expo-router';
+// import { getAuth } from 'firebase/auth';
+// import { collection, getDocs, query, where } from 'firebase/firestore';
+// import { useEffect, useState } from 'react';
+// import {
+//   ActivityIndicator,
+//   Alert,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from 'react-native';
+// import { db } from '../../firebase/config';
+
+
+// export default function CustomerHome() {
+//   const router = useRouter();
+//   const [customerId, setCustomerId] = useState('');
+//   const [loading, setLoading] = useState(true);
+
+
+//   useEffect(() => {
+//     const fetchCustomerDoc = async () => {
+//       try {
+//         const currentUser = getAuth().currentUser;
+//         if (!currentUser) {
+//           Alert.alert('Error', 'Customer not logged in');
+//           return;
+//         }
+
+
+//         const q = query(
+//           collection(db, 'customers'),
+//           where('email', '==', currentUser.email)
+//         );
+
+
+//         const snapshot = await getDocs(q);
+
+
+//         if (!snapshot.empty) {
+//           const doc = snapshot.docs[0];
+//           setCustomerId(doc.id);
+//         } else {
+//           Alert.alert('Not Found', 'Customer data not found');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching customer:', error);
+//         Alert.alert('Error', 'Could not load customer data');
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+
+//     fetchCustomerDoc();
+//   }, []);
+
+
+//   const handleViewStatus = () => {
+//     if (customerId) {
+//       router.push(`/customer/status/${customerId}`);
+//     } else {
+//       Alert.alert('Error', 'Customer ID not found');
+//     }
+//   };
+
+
+//   const handleViewBill = () => {
+//     if (customerId) {
+//       router.push(`/customer/bill/${customerId}`);
+//     } else {
+//       Alert.alert('Error', 'Customer ID not found');
+//     }
+//   };
+
+
+//   const handleSelectTechnician = () => {
+//     router.push('/customer/AreaSelection');
+//   };
+
+
+//   const handleFeedback = () => {
+//    // router.push('/customer/feedback');
+//   };
+
+
+//   if (loading) {
+//     return (
+//       <View style={styles.center}>
+//         <ActivityIndicator size="large" color="#283593" />
+//         <Text style={{ marginTop: 10 }}>Loading your information...</Text>
+//       </View>
+//     );
+//   }
+
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.heading}>Welcome to GramUrja ⚡</Text>
+
+
+//       <TouchableOpacity style={styles.button} onPress={handleViewStatus}>
+//         <Text style={styles.buttonText}>📍 View Installation Status</Text>
+//       </TouchableOpacity>
+
+
+//       <TouchableOpacity style={styles.billButton} onPress={handleViewBill}>
+//         <Text style={styles.billText}>🧾 View Bill</Text>
+//       </TouchableOpacity>
+
+
+//       <TouchableOpacity style={styles.selectButton} onPress={handleSelectTechnician}>
+//         <Text style={styles.selectText}>👷 Select Technician</Text>
+//       </TouchableOpacity>
+
+
+//       <TouchableOpacity style={styles.feedbackButton} onPress={handleFeedback}>
+//         <Text style={styles.feedbackText}>💬 Give Feedback</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 30,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: '#F5F5F5',
+//   },
+//   heading: {
+//     fontSize: 22,
+//     fontWeight: 'bold',
+//     marginBottom: 40,
+//     textAlign: 'center',
+//     color: '#333',
+//   },
+//   button: {
+//     backgroundColor: '#283593',
+//     paddingVertical: 15,
+//     paddingHorizontal: 30,
+//     borderRadius: 10,
+//     marginBottom: 20,
+//     width: '80%',
+//     alignItems: 'center',
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//   },
+//   billButton: {
+//     backgroundColor: '#4648bdff',
+//     paddingVertical: 15,
+//     paddingHorizontal: 30,
+//     borderRadius: 10,
+//     marginBottom: 20,
+//     width: '80%',
+//     alignItems: 'center',
+//   },
+//   billText: {
+//     color: '#fff',
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//   },
+//   selectButton: {
+//     backgroundColor: '#393febff',
+//     paddingVertical: 14,
+//     paddingHorizontal: 30,
+//     borderRadius: 10,
+//     marginBottom: 20,
+//     width: '80%',
+//     alignItems: 'center',
+//   },
+//   selectText: {
+//     color: '#fff',
+//     fontSize: 15,
+//     fontWeight: 'bold',
+//   },
+//   feedbackButton: {
+//     borderColor: '#283593',
+//     borderWidth: 1.5,
+//     paddingVertical: 14,
+//     paddingHorizontal: 30,
+//     borderRadius: 10,
+//     width: '80%',
+//     alignItems: 'center',
+//   },
+//   feedbackText: {
+//     color: '#283593',
+//     fontSize: 15,
+//     fontWeight: 'bold',
+//   },
+//   center: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
+
+
+
+
+
+
 import { useRouter } from 'expo-router';
 import { getAuth } from 'firebase/auth';
 import { collection, getDocs, query, where } from 'firebase/firestore';
@@ -268,7 +484,7 @@ export default function CustomerHome() {
 
 
   const handleFeedback = () => {
-   // router.push('/customer/feedback');
+   router.push('/customer/feedback');
   };
 
 
@@ -387,6 +603,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
